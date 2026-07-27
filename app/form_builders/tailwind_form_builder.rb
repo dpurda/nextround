42501c8563094c0ffcdd9e945ecf8a613ae7e7d2
dpurda <purdaioandaniel@gmail.com>
2026-07-27 14:33:55 +0300

    Add TailwindFormBuilder for enhanced form styling and input handling

---
+++ app/form_builders/tailwind_form_builder.rb
@@ -0,0 +1,18 @@
+class TailwindFormBuilder < ActionView::Helpers::FormBuilder
+  INPUT_CLASS = "mt-1 block w-full rounded-md border-slate-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+  LABEL_CLASS = "block text-sm font-medium text-slate-700"
+
+  %i[text_field email_field password_field number_field text_area].each do |method_name|
+    define_method(method_name) do |attribute, options = {}|
+      options[:class] ||= INPUT_CLASS
+      super(attribute, options)
+    end
+  end
+
+  # Renders a label + input as a single unit, e.g. f.labeled_field :phone
+  def labeled_field(attribute, label_text = nil, as: :text_field, **options)
+    @template.content_tag(:div) do
+      label(attribute, label_text, class: LABEL_CLASS) + public_send(as, attribute, options)
+    end
+  end
+end
